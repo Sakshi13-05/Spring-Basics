ab18188945a3e848e888a0a0514f121b399363ea
Sakshi13-05 <sakshichavan611162gmail.com>
2026-09-16 10:29:27 +0530

    Day1--Practising basics of spring--Understanding linkage between Entity and Repository

--- practice/src/main/java/com/example/practice/exception/CourseNotFound.java
+++
@@ -1,8 +0,0 @@
-package com.example.practice.exception;
-
-public class CourseNotFound extends RuntimeException {
-    public CourseNotFound(String message) {
-        super(message);
-    }
-
-}
--- practice/src/main/java/com/example/practice/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.practice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.practice.entity.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    // Spring Data generates findAll(), findById(), save(), delete() automatically!
-}
