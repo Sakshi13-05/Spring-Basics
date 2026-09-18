61007459ead33dd47e9e73143720d4f0ea23a860
Sakshi13-05 <sakshichavan61116@gmail.com>
2026-09-18 14:50:13 +0530

    Revising the entity and repository

--- practice/src/main/java/com/example/practice/PracticeApplication.java
+++ practice/src/main/java/com/example/practice/PracticeApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.example.practice.model.Product;
-import com.example.practice.repository.ProductRepo;
-
 import lombok.Data;
 import lombok.ToString;
 
@@ -17,21 +14,6 @@ public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
-	}
-
-	@Bean
-
-	public CommandLineRunner demo(ProductRepo repo) {
-		return (args) -> {
-			repo.save(new Product("shampoo", 100.0, 15));
-			repo.save(new Product("soap", 100.0, 156));
-			System.out.println("The database content are:");
-			System.out.println(repo.getPdtByName("shampoo").get());
-			// for (Product pdt : repo.findAll()) {
-			// System.out.println(pdt.getId() + " " + pdt.getName() + " -->" +
-			// pdt.getPrice());
-			// }
-		};
 	}
 
 }
